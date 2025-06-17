7bd2f993e859c1f25a9bd9521f208fc2236c7a4c
Aldo Espinosa <aldoespinosaperez1@gmail.com>
2025-06-16 18:08:00 -0600

    Palindrome Number

---
+++ src/main/java/PalindromeNumber.java
@@ -0,0 +1,25 @@
+/***
+ * Given an integer x, return true if x is a palindrome, and false otherwise.
+ */
+
+public class PalindromeNumber {
+  public static void main(String[] args) {
+    System.out.println(isPalindrome(121)); // true
+    System.out.println(isPalindrome(-121)); // false
+    System.out.println(isPalindrome(10)); // false
+  }
+
+  public static boolean isPalindrome(int x) {
+    if (x < 0 || (x % 10 == 0 && x != 0)) {
+      return false;
+    }
+
+    int reversed = 0;
+    while (x > reversed) {
+      reversed = reversed * 10 + x % 10;
+      x /= 10;
+    }
+
+    return x == reversed || x == reversed / 10;
+  }
+}
